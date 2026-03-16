8f13137e31854a8da2de7d6c0177d51d07b1a900
kcym-paduppu-official <princetmprince@gmail.com>
2026-03-16 10:10:47 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+export const metadata = {
+title:"KCYM Padup Unit 2025-26",
+description:"Bus Timetable"
+}
+
+export default function RootLayout({children}){
+
+return(
+
+<html lang="ml">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+)
+
+}
