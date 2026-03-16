ca0541819c1e4b11e663912e1dfd0657b2c9e994
kcym-paduppu-official <princetmprince@gmail.com>
2026-03-16 10:12:09 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,31 @@
+import BusTable from "../components/BusTable"
+
+export default function Home(){
+
+return(
+
+<div className="container">
+
+<header className="header">
+
+<h1>കെസിവൈഎം പടുപ്പ് യൂണിറ്റ് 2025-26</h1>
+
+<p>Padup Bus Timetable</p>
+
+</header>
+
+<BusTable/>
+
+<footer className="footer">
+
+<p>KCYM Padup Unit</p>
+
+<p>Bus Timetable 2025-26</p>
+
+</footer>
+
+</div>
+
+)
+
+}
